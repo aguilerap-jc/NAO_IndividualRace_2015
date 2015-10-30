d0c06fac05c7174a93e0fb6c8720bcf14e38a840
Fernando López <fer_fjsb2@hotmail.com>
2015-10-30 00:43:07 -0600

    Delete folder

---
+++ build-naoki-sdk1.14/CMakeFiles/individualrace.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/individualrace.dir/main.cpp.o"
+  "sdk/bin/individualrace.pdb"
+  "sdk/bin/individualrace"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/individualrace.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
