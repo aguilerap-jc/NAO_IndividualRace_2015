5940920e8d76796bca3b2c3ae3052c42b3a6549a
AguilerapJC <aguilerap.jc@gmail.com>
2015-11-09 10:17:26 -0600

    sin cambios

---
+++ build-naoqi64/CMakeFiles/test_wlkv2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/test_wlkv2.dir/CaminadoNAO.cpp.o"
+  "sdk/bin/test_wlkv2.pdb"
+  "sdk/bin/test_wlkv2"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/test_wlkv2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
